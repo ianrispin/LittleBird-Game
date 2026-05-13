1332eea96ea0c2d1145248a3b788ddd80ef7b9f7
glorie16 <gcmendoza@cpp.edu>
2026-05-12 20:43:29 -0700

    Added healthbars for player and enemy ai

--- Little Bird/Assets/Scripts/Health.cs
+++ Little Bird/Assets/Scripts/Health.cs
@@ -1,23 +1,80 @@
 using UnityEngine;
+using Microlight.MicroBar;
+
 public class Health : MonoBehaviour
 {
-[SerializeField] float maxHealth = 100f;
-float currentHealth;
-void Start()
-{
-currentHealth = maxHealth;
-}
-public void TakeDamage(float amount)
-{
-currentHealth -= amount;
-Debug.Log(gameObject.name + " health: " + currentHealth);
-if (currentHealth <= 0)
-{
-Die();
-}
-}
-void Die()
-{
-Destroy(gameObject);
-}
+    [SerializeField] float maxHealth = 100f;
+    public float currentHealth;
+
+    [Header("Player Health Bar")]
+    public MicroBar healthBar;
+
+    [Header("Enemy Health Bar")]
+    public GameObject healthBarPrefab;
+    public Vector3 barOffset = new Vector3(0, 0.8f, 0);
+    private MicroBar enemyBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+        if (healthBar != null)
+        {
+            healthBar.Initialize(maxHealth);
+        }
+        else if (healthBarPrefab != null)
+        {
+            GameObject canvasGO = new GameObject("EnemyBarCanvas");
+            Canvas canvas = canvasGO.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.WorldSpace;
+            canvasGO.transform.SetParent(transform);
+            canvasGO.transform.localPosition = barOffset;
+            canvasGO.transform.localScale = new Vector3(0.005f, 0.005f, 0.005f);
+
+            GameObject bar = Instantiate(healthBarPrefab, canvasGO.transform);
+            bar.transform.localPosition = Vector3.zero;
+            enemyBar = bar.GetComponent<MicroBar>();
+            enemyBar.Initialize(maxHealth);
+        }
+    }
+
+    void Update()
+    {
+        if (enemyBar != null)
+        {
+            enemyBar.transform.parent.LookAt(Camera.main.transform);
+            enemyBar.transform.parent.Rotate(0, 180, 0);
+        }
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        Debug.Log(gameObject.name + " health: " + currentHealth);
+
+        if (healthBar != null)
+            healthBar.UpdateBar(currentHealth);
+
+        if (enemyBar != null)
+            enemyBar.UpdateBar(currentHealth);
+
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    public void RestoreHealth(float amount)
+    {
+        currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
+
+        if (healthBar != null)
+            healthBar.UpdateBar(currentHealth);
+
+        if (enemyBar != null)
+            enemyBar.UpdateBar(currentHealth);
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
 }
